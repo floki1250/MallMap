c4532e8c562bf413d6ab81efcd1c7dde5bb247ee
floki1250 <d.adem1250@gmail.com>
2024-04-15 21:52:26 +0100

    page

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,15 @@
     <div class="flex justify-center">
         <svg width="590" height="373" viewBox="0 0 590 373" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g id="MallMap">
-                <NuxtLink to="https://www.carrefour.tn/" id="14">
-                    <path id="14" d="M431 29.5H589.5V241.5H56.5V29.5H263.5H264V29V1H322.5H430.5V29V29.5H431Z"
-                        fill="#134889" stroke="white" />
+                <NuxtLink to="store/14" id="14">
+                    <g id="14">
+                        <path id="14_2" d="M431 29.5H589.5V241.5H56.5V29.5H263.5H264V29V1H322.5H430.5V29V29.5H431Z"
+                            fill="#134889" stroke="white" />
+                        <text id="Carrefour" fill="white" xml:space="preserve" style="white-space: pre"
+                            font-family="Josefin Sans" font-size="40" letter-spacing="0em">
+                            <tspan x="236.352" y="131">Carrefour</tspan>
+                        </text>
+                    </g>
                 </NuxtLink>
 
                 <NuxtLink to="store/15" id="15">
--- pages/store/[id].vue
+++ pages/store/[id].vue
@@ -1,15 +1,19 @@
 <template>
     <div>
-        <iframe :src="data[route.params.id].url" frameborder="0" class="w-full h-screen"></iframe>
+        {{ route.params.id }}
+        <svg width="590" height="373" viewBox="0 0 590 373" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="14">
+                <path id="14_2" d="M431 29.5H589.5V241.5H56.5V29.5H263.5H264V29V1H322.5H430.5V29V29.5H431Z"
+                    fill="#134889" stroke="white" />
+                <text id="Carrefour" fill="white" xml:space="preserve" style="white-space: pre"
+                    font-family="Josefin Sans" font-size="40" letter-spacing="0em">
+                    <tspan x="236.352" y="131">Carrefour</tspan>
+                </text>
+            </g>
+        </svg>
     </div>
 </template>
 
 <script setup>
 const route = useRoute();
-const data = {
-    14: {
-        name: "Carrefour",
-        url: "https://www.carrefour.tn/",
-    },
-};
 </script>
